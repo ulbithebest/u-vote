e4f31b9b6edd9d6eae287815dcd2ed711adfc479
Gading Khairlambang <sasakihaise985@gmail.com>
2024-03-11 15:03:06 +0700

    upgrade homepage

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,28 +1,16 @@
-// function dropDown() {
-//     document.querySelectorAll(".submenu").classList.
-//     document.querySelectorAll('.arrow').classList.toggle('rotate-0')
-//   }
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownButton = document.getElementById('user-menu-button');
+    var dropdownMenu = document.getElementById('isi_dropdown');
 
-
-// dropDown()
-
-let submenu = document.querySelectorAll(".coba")
-let lah = document.querySelectorAll(".submenu")
-let arrows = document.querySelectorAll(".arrow")
-
-function test() {
-    for (let i=0; i < submenu.length; i++) {
-        lah[i].onclick = function () {
-            submenu[i].classList.toggle('hidden')
-            arrows[i].classList.toggle('rotate-0')
-        }
-    }
-}
-
-test()
-
-  function Openbar() {
-    document.querySelector('.sidebar').classList.toggle('left-[-300px]')
-}
-
-
+    dropdownButton.addEventListener('click', function () {
+      dropdownMenu.classList.toggle('hidden');
+    });
+  
+    document.addEventListener('click', function (event) {
+      var isClickInside = dropdownButton.contains(event.target) || dropdownMenu.contains(event.target);
+      if (!isClickInside) {
+        dropdownMenu.classList.add('hidden');
+      }
+    });
+  });
+  
